2b9e7a8c3fed6509dbc6f25d05704da82f05c3f3
Anton Agestam <msn@antonagestam.se>
2012-06-20 19:37:01 -0400

    removed client id and secret

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-	define('TRIPL_CLIENT_ID','4fe14549e046251762000045');
-	define('TRIPL_CLIENT_SECRET','33ba97ee45d4978fc02a71d8a9d77c27066442bd4fa6c36e697334a8af1145be');
-	define('TRIPL_API_BASE','http://anton.dev.tripl.com/api/v1/');
+	define('TRIPL_CLIENT_ID',		'ENTER_CLIENT_ID_HERE');
+	define('TRIPL_CLIENT_SECRET',	'ENTER_CLIENT_SECRET_HERE');
+	define('TRIPL_API_BASE',		'http://tripl.com/api/v1/');
 
 	function __autoload( $class ){
 		require_once( lcfirst($class) . ".php" );
